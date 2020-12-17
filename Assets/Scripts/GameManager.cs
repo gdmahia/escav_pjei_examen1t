62bfa1e68c98af3732b59c73d643835c2d0b0474
gdmahia <73638645+gdmahia@users.noreply.github.com>
2020-12-17 15:55:47 +0100

    Revert "Merge branch 'pausa' into Develop"
    This reverts commit d632d6d08c1b45c71e11783bf0484314a10be898, reversing
    changes made to a42fffc7dd945a3a6b17272dd84c14ee89428deb.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,10 +18,6 @@ public class GameManager : MonoBehaviour {
     [SerializeField]
     private GameObject levelFinishedPanel;
 
-    [Tooltip("Referencia al panel de pausa")]
-    [SerializeField]
-    private GameObject pausePanel;
-
     // Propiedad que almacena el número de monedas recolectadas por el jugador
     private int coins;
     public static int Coins {
@@ -39,18 +35,6 @@ public class GameManager : MonoBehaviour {
     private void Awake() {
         /* Singleton (se da por hecho que nunca habrá más de una instancia del GameManager) */
         instance = this;
-    }
-
-    private void Start() {
-        // Se asegura que el juego no comienza pausado
-        Time.timeScale = 1;
-    }
-
-    private void Update() {
-        // Botón de pausa
-        if (Input.GetKeyDown(KeyCode.Escape)) {
-            PauseButtonPressed();
-        }
     }
 
     #endregion
@@ -74,16 +58,5 @@ public class GameManager : MonoBehaviour {
     private void LoadGame() {
         // Carga la escena del juego
         SceneManager.LoadScene(0);
-    }
-
-    private void PauseButtonPressed() {
-        // Comprueba si el juego estaba pausado o en ejecución para cambiar su estado
-        if (Time.timeScale == 1) {
-            pausePanel.SetActive(true);
-            Time.timeScale = 0;
-        } else {
-            pausePanel.SetActive(false);
-            Time.timeScale = 1;
-        }
     }
 }
